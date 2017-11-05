b1da8044f57fc466d6699cf277335e1c6269def7
Alex Fernández <alexfernandeznpm@gmail.com>
2017-11-06 00:25:42 +0100

    Server.

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const http = require('http');
+const exec = require('child_process')
+
+function start() {
+  const server = http.createServer((request, response) => {
+    console.log('Received request for %s', request.url)
+    exec('cd ~/test/ && git pull', (error, stdout) => {
+      if (error) return console.error('Could not update: %s', error);
+      console.log('Updated: %s', stdout);
+    });
+  });
+  server.listen(() => {
+    console.log('Server listening');
+  });
+}
+
+start();
+
